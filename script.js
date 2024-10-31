0b4ef1b64b85f1411d12f82242089eeb7eedee45
refi-rr <refito.backup002@gmail.com>
2024-10-31 13:33:38 +0700

    Create script.js
    For load test k6

---
+++ script.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 30, // Jumlah virtual users
+    duration: '2m', // Durasi test
+};
+
+export default function () {
+    const headers = {
+        'Content-Type': 'text/plain',
+        'Authorization': 'Basic ' + btoa(`${__ENV.USERNAME}:${__ENV.PASSWORD}`)
+    };
+    const response = http.post(`${__ENV.CLICKHOUSE_URL}`, 'SELECT COUNT(*) FROM my_table', { headers });
+
+    check(response, {
+        'status is 200': (r) => r.status === 200,
+        'response time < 200ms': (r) => r.timings.duration < 200,
+    });
+
+    sleep(1);
+}
